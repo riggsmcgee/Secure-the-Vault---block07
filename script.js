51c5ae3c26a6c48a12d7aa33010a8542c522953b
Riggs Gorman <riggsgorman@Riggss-MacBook-Pro.local>
2024-05-28 19:54:46 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+//You have received this message because you have been chosen to open an important vault. Here is the secret combination: 10-40-39
+const comboMessage = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+// calculate combo 10
+const comboOne = 4*8-66/3
+
+
+// calculate combo 40
+const comboTwo = 3587/17-11*10-61
+
+
+// calculate combo 39
+const comboThree = comboTwo - 1
+
+// alert ("I am alert!")
+
+//alert(`${comboMessage}\n${comboOne} - ${comboTwo} - ${comboThree} \n \n Good luck, Harry Potter...`);
+
+console.log(`${comboMessage}\n${comboOne} - ${comboTwo} - ${comboThree} \n \n Good luck, Harry Potter...`);
